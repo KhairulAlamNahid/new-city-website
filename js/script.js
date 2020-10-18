71f672b42468aba7848674a90d1bf1351e9140a5
KhairulAlamNahid <profreelancer10@gmail.com>
2020-10-19 00:35:23 +0600

    done

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// banner part slider js
+
+$('#banner_part').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa fa-hand-o-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-hand-o-right next" aria-hidden="true"></i>',
+    arrows: true,
+    autoplay: true,
+    fade: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+//about venobox js 
+$('.venobox').venobox();
+
+
+// coutner up js
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
